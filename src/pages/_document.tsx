e2749e10a3ee1df32ea80ebd1593d5aa6b80348d
Max Humpherys <maxhumpherys@gmail.com>
2023-03-26 18:55:09 -0600

    Imported Manrope 400 & 700, Closes #13

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,16 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import dynamic from 'next/dynamic';
 
-
-const NavComponent = dynamic(() =>
-  import('../components/Nav'))
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
-        <NavComponent />
         <Main />
         <NextScript />
       </body>
