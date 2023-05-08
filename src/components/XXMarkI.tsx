4b3d2ca34dfff949975dd4af57c764e9a77178f0
Max Humpherys <maxhumpherys@gmail.com>
2023-05-08 15:44:39 -0600

    XX99 Mark I component. Closes #31

---
+++ src/components/XXMarkI.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+
+function PagePreview(): JSX.Element {
+    return <Image src="/assets/product-xx99-mark-one-headphones/mobile/image-category-page-preview.jpg" className='rounded-lg' alt="earphones" width={372} height={352} />
+}
+
+const XXMarkI = (): JSX.Element => {
+    return (
+
+        <div className='mx-[24px] h-[724px]'>
+            <div>
+                {PagePreview()}
+            </div>
+            <div className='flex flex-col items-center justify-end my-2'>
+                <h2 className='font-bold text-black text-center text-3xl tracking-[1px] mt-4'>XX99 MARK I<br />HEADPHONES</h2>
+                <p className='opacity-50 font-medium text-lg text-black text-center px-2 my-4'>As the gold standard for headphones, the classic XX99 Mark I offers detailed and accurate audio reproduction for audiophiles, mixing engineers, and music aficionados alike in studios and on the go.</p>
+                <button className='bg-[#D87D4A] font-bold px-6 py-4 my-'>SEE PRODUCT</button>
+            </div>
+        </div>
+    )
+}
+
+export default XXMarkI;
--- src/pages/headphones/index.tsx
+++ src/pages/headphones/index.tsx
@@ -4,9 +4,13 @@ const XXMarkII = dynamic(() =>
     import('../../components/XXMarkII')
 )
 
+const XXMarkI = dynamic(() =>
+    import('../../components/XXMarkI')
+)
+
 const Headphones = (): JSX.Element => {
     return (
-        <div>
+        <div className=''>
             <header className="h-[102px]  flex justify-center items-center">
                 <h1 className="font-bold text-2xl tracking-[2px]">
                     HEADPHONES
@@ -15,6 +19,10 @@ const Headphones = (): JSX.Element => {
 
             <section className='bg-white py-12'>
                 <XXMarkII />
+            </section>
+
+            <section className='bg-white py-12'>
+                <XXMarkI />
             </section>
         </div>
     )
