6c0739de6b8beec183cfc8cd3ee6bf743ebd75ee
Max Humpherys <maxhumpherys@gmail.com>
2023-05-09 15:27:00 -0600

    YX1 Earphones component and Earphones page. Closes #39

--- src/pages/earphones/index.tsx
+++ src/pages/earphones/index.tsx
@@ -1,8 +1,38 @@
+import dynamic from 'next/dynamic';
+
+
+const YX1Earphones = dynamic(() =>
+    import('../../components/Earphones/YX1Earphones')
+)
+
+const Menu = dynamic(() =>
+    import('../../components/Menu'))
+
+const Footer = dynamic(() =>
+    import('../../components/Footer')
+)
+
 const Earphones = () => {
     return (
-        <>
-            <h2>Earphones page</h2>
-        </>
+        <div>
+            <header className="h-[102px]  flex justify-center items-center">
+                <h1 className="font-bold text-2xl tracking-[2px]">
+                    EARPHONES
+                </h1>
+            </header>
+
+            <section className='bg-white py-12'>
+                <YX1Earphones />
+            </section>
+
+            <section className='bg-white py-12'>
+                <Menu />
+            </section>
+
+            <section className='bg-white pt-12'>
+                <Footer />
+            </section>
+        </div>
     )
 }
 
