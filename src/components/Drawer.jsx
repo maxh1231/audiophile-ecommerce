3c1bf37b4de9c9dbdeead5d5f422e60aa4bd7bf0
maxh1231 <maxhumpherys@gmail.com>
2023-07-18 14:04:16 -0600

    rm files

--- src/components/Drawer.jsx
+++
@@ -1,9 +0,0 @@
-import React, { forwardRef } from 'react';
-const Drawer = forwardRef((props, ref) => {
-    return (
-        <menu className="dropdown" ref={ref}>
-            {props.children}
-        </menu>
-    );
-});
-export default Drawer;
