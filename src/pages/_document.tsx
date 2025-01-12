240610dc7a2bc9a43ca43802bd6e310f1b85e047
MaxHumpherys <maxhumpherys@gmail.com>
2025-01-11 21:55:04 -0700

    title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <title>audiophile</title>
       <body className="bg-[#191919]">
         <Main />
         <NextScript />
